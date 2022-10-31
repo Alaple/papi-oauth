0eb9d61f4d1f9de0186c0ac0b3d37bb3cdeb9e7c
LucasPalaciosDistillery <lucas.palacios@distillery.com>
2022-10-31 17:56:16 -0300

    Add node.js OAuth

---
+++ index.js
@@ -0,0 +1,66 @@
+/*  EXPRESS */
+const express = require('express');
+const app = express();
+const data = require('./data.json')
+
+app.set('view engine', 'ejs');
+var access_token = "";
+
+app.get('/', function(req, res) {
+  res.render('pages/index',{client_id: clientID});
+});
+
+const port = process.env.PORT || 2400;
+app.listen(port , () => console.log('App listening on port ' + port));
+
+// Import the axios library, to make HTTP requests
+const axios = require('axios')
+// This is the client ID and client secret that you obtained
+// while registering on github app
+const clientID = 'f99b9221a8bd0082c463'
+const clientSecret = 'c3e63b184223e18cc6cc3fe78e0fa0cbb16e3a01'
+
+// Declare the callback route
+app.get('/github/callback', (req, res) => {
+
+  // The req.query object has the query params that were sent to this route.
+  const requestToken = req.query.code
+  
+  axios({
+    method: 'post',
+    url: `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}`,
+    // Set the content type header, so that we get the response in JSON
+    headers: {
+         accept: 'application/json'
+    }
+  }).then((response) => {
+    access_token = response.data.access_token
+    res.redirect('/success');
+  })
+})
+
+app.get('/success', function(req, res) {
+  axios({
+    method: 'get',
+    url: `https://api.github.com/user`,
+    headers: {
+      Authorization: 'token ' + access_token
+    }
+  }).then((response) => {
+    access_token = response.data.access_token
+    res.render('pages/success',{ userData: response.data });
+    try {
+        const response = axios.post('https://privacyapi.brandyourself.com/v1/scans',data,
+        {
+            headers: {
+            Authorization: access_token
+            }
+        })
+
+        console.log('Response:', response.data)
+    } catch (error) {
+        console.error(error)
+    }
+    
+  })
+});
